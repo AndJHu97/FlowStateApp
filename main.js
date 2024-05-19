5f139c35b023952f5c57dad757cfae5b8212baf3
AndJHu97 <Andrewhu1997@gmail.com>
2024-05-18 22:40:55 -0500

    idk

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const {app, BrowserWindow} = require('electron');
 const url = require('url');
 const path = require('path');
+const isDev = !app.isPackaged;
 
 function createMainWindow(){
     const mainWindow = new BrowserWindow({
         title: "Flow State",
         width: 1500,
-        height: 800
+        height: 800,
+        webPreferences:{
+            preload: path.join(__dirname, 'preload.js'),
+        }
     });
 
+    mainWindow.webContents.openDevTools();
+
+    const startUrl = url.format({
+        pathname: path.join(__dirname, './FlowReact/build/index.html'),
+        protocol: 'file'
+    });
     // Load the HTML file
-    mainWindow.loadFile(path.join(__dirname, 'index.html'));
+    //if development then localhost, when production runs the url
+    mainWindow.loadURL(
+        isDev
+        ? 'http://localhost:3000':
+        startUrl
+        );
 }
 
 
 app.whenReady().then(createMainWindow);
- 
